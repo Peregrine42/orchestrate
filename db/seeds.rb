d2f448a6357e24c25d3b71e28e221f6b662185e5
Duncan Fermor <duncan@elitemail.org>
2016-01-23 21:03:22 +0000

    updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,30 @@ DatabaseCleaner.strategy = :truncation
 DatabaseCleaner.clean!
 
 Order.create(
-  first_name: "Duncan",
-  last_name: "Fermor",
+  first_name: "Ford",
+  last_name: "Prefect",
   address_line_1: "somewhere",
   address_line_2: "far away",
-  postcode: "CO11 1SF",
+  postcode: "XX4 1SD",
   status: "pending"
+)
+
+Order.create(
+  first_name: "Arthur",
+  last_name: "Dent",
+  address_line_1: "somewhere",
+  address_line_2: "far away",
+  postcode: "XX4 1SD",
+  status: "confirmed"
+)
+
+Order.create(
+  first_name: "Zaphod",
+  last_name: "Beeblebrox",
+  address_line_1: "somewhere",
+  address_line_2: "far away",
+  postcode: "XX4 1SD",
+  status: "dispatched"
 )
 
 Product.create(
